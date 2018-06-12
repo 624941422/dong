ccb6bab56a2b4be88cb329e7b3d28a56fd8e7275
624941422@qq.com <624941422@qq.com>
2018-06-12 23:19:38 +0800

    'change'

--- myadmin/modules/RegisterAdmin.py
+++ myadmin/modules/RegisterAdmin.py
@@ -12,6 +12,6 @@ def register(models_class, admin_class=None):
     app_name = models_class._meta.app_label
     if app_name not in register_dic:
         register_dic[app_name] = {}
-    register_dic[app_name][models_class] = admin_class
-
+    register_dic[app_name][models_class._meta.object_name] = admin_class
 register(models.Customer, CustomerAdmin)
+register(models.UserProfile)
